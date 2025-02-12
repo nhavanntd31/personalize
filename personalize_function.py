808ed1a41af61441c07ca09c6604064e5577afe3
DatNguyen3 <dat.nguyen3@ekotek.vn>
2025-02-12 15:11:18 +0700

    fix

--- personalize_function.py
+++ personalize_function.py
@@ -18,7 +18,15 @@ def convert_to_32bit_float(input_wav, output_wav):
   print(f"output_wav_type: {type(output_wav)}")
   print(f"input_wav_data_type: {input_wav.dtype}")
   print(f"output_wav_data_type: {output_wav.dtype}")
-
+  print(f"input_wav_data_type: {input_wav.dtype}")
+  print(f"output_wav_data_type: {output_wav.dtype}")  
+  print(f"input_wav_data_type: {input_wav.dtype}")
+  print(f"output_wav_data_type: {output_wav.dtype}")
+  print(f"input_wav_data_type: {input_wav.dtype}")
+  print(f"output_wav_data_type: {output_wav.dtype}")
+  print(f"input_wav_data_type: {input_wav.dtype}")
+  print(f"output_wav_data_type: {output_wav.dtype}")
+  
   try:
     fs, data = wavfile.read(input_wav)
   except FileNotFoundError:
